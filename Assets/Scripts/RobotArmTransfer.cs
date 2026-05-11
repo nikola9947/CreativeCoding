e51bc52c50988a900c7f45ad7480608a0bb85c87
nikola9947 <nikolabryks1999@gmail.com>
2026-05-11 11:26:38 +0200

    Animationen eingefügt

--- Assets/Scripts/RobotArmTransfer.cs
+++ Assets/Scripts/RobotArmTransfer.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 
 public class RobotArmTransfer : MonoBehaviour
 {
+    [Header("Transfer")]
     public Transform dropPoint;
+
+    [Header("Conveyor")]
     public ConveyorBelt inputConveyor;
 
+    [Header("Robot Animation")]
+    public Animator robotAnimator;
+    public string workTriggerName = "Work";
+    public float animationLength = 0.867f;
+
+    [Header("Movement")]
     public float liftHeight = 1.5f;
-    public float moveDuration = 1f;
+    public float moveDuration = 0.5f;
 
     private bool isMoving = false;
-    private const string ROBOT_REASON = "ROBOT_BUSY";
+    private const string ROBOT_BUSY_REASON = "ROBOT_BUSY";
 
     private void OnTriggerEnter(Collider other)
     {
@@ -19,17 +28,37 @@ public class RobotArmTransfer : MonoBehaviour
 
         if (isMoving)
         {
-            inputConveyor.AddStopReason(ROBOT_REASON);
+            if (inputConveyor != null)
+                inputConveyor.AddStopReason(ROBOT_BUSY_REASON);
+
             return;
         }
 
-        StartCoroutine(MovePackage(other.gameObject));
+        StartCoroutine(TransferPackage(other.gameObject));
     }
 
-    private IEnumerator MovePackage(GameObject package)
+    private IEnumerator TransferPackage(GameObject package)
     {
         isMoving = true;
-        inputConveyor.AddStopReason(ROBOT_REASON);
+
+        if (inputConveyor != null)
+            inputConveyor.AddStopReason(ROBOT_BUSY_REASON);
+
+        if (robotAnimator != null)
+        {
+            float totalMoveTime = moveDuration * 3f;
+
+            robotAnimator.speed = animationLength / totalMoveTime;
+
+            robotAnimator.ResetTrigger(workTriggerName);
+            robotAnimator.SetTrigger(workTriggerName);
+
+            Debug.Log("Robot trigger fired!");
+        }
+        else
+        {
+            Debug.LogWarning("Robot Animator missing!");
+        }
 
         Rigidbody rb = package.GetComponent<Rigidbody>();
 
@@ -46,20 +75,29 @@ public class RobotArmTransfer : MonoBehaviour
         Vector3 liftPos = startPos + Vector3.up * liftHeight;
         Vector3 dropLiftPos = dropPoint.position + Vector3.up * liftHeight;
 
-        Quaternion uprightRotation = Quaternion.Euler(90f, dropPoint.eulerAngles.y, 0f);
+        Quaternion uprightRotation = Quaternion.Euler(
+            90f,
+            dropPoint.eulerAngles.y,
+            0f
+        );
 
-        yield return Move(package.transform, startPos, liftPos, startRot, startRot);
-        yield return Move(package.transform, liftPos, dropLiftPos, startRot, uprightRotation);
-        yield return Move(package.transform, dropLiftPos, dropPoint.position, uprightRotation, uprightRotation);
+        yield return MoveObject(package.transform, startPos, liftPos, startRot, startRot);
+        yield return MoveObject(package.transform, liftPos, dropLiftPos, startRot, uprightRotation);
+        yield return MoveObject(package.transform, dropLiftPos, dropPoint.position, uprightRotation, uprightRotation);
 
         if (rb != null)
             rb.isKinematic = false;
 
+        if (robotAnimator != null)
+            robotAnimator.speed = 1f;
+
         isMoving = false;
-        inputConveyor.RemoveStopReason(ROBOT_REASON);
+
+        if (inputConveyor != null)
+            inputConveyor.RemoveStopReason(ROBOT_BUSY_REASON);
     }
 
-    private IEnumerator Move(
+    private IEnumerator MoveObject(
         Transform obj,
         Vector3 fromPos,
         Vector3 toPos,
