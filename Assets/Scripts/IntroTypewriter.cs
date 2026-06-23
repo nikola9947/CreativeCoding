e0e58b940a7a680176e3e5d104284ca09b2e58b1
nikola9947 <nikolabryks1999@gmail.com>
2026-06-23 15:48:58 +0200

    Titelbild eingefügt

--- Assets/Scripts/IntroTypewriter.cs
+++ Assets/Scripts/IntroTypewriter.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class IntroTypewriter : MonoBehaviour
 {
-    public GameObject introCanvas;
+    [Header("Panels")]
+    public GameObject titlePanel;
+    public GameObject introPanel;
+    public GameObject mainCanvas;
+
+    [Header("Intro")]
     public TextMeshProUGUI introText;
     public GameObject startButton;
 
+    [Header("Game")]
     public GameManager gameManager;
 
     [TextArea(4, 10)]
@@ -18,23 +25,75 @@ public class IntroTypewriter : MonoBehaviour
 
     public float typingSpeed = 0.03f;
 
-    private bool isTyping = true;
+    private bool titleActive = true;
+    private bool introActive = false;
+    private bool isTyping = false;
+    private bool gameStarted = false;
 
     private void Start()
     {
+        if (titlePanel != null)
+            titlePanel.SetActive(true);
+
+        if (introPanel != null)
+            introPanel.SetActive(false);
+
+        if (mainCanvas != null)
+            mainCanvas.SetActive(false);
+
         if (startButton != null)
             startButton.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (gameStarted)
+            return;
+
+        if (Mouse.current == null)
+            return;
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            if (titleActive)
+            {
+                ShowIntro();
+                return;
+            }
+
+            if (introActive && !isTyping)
+            {
+                StartGame();
+            }
+        }
+    }
+
+    private void ShowIntro()
+    {
+        titleActive = false;
+        introActive = true;
+
+        if (titlePanel != null)
+            titlePanel.SetActive(false);
+
+        if (introPanel != null)
+            introPanel.SetActive(true);
 
         StartCoroutine(TypeText());
     }
 
     private IEnumerator TypeText()
     {
-        introText.text = "";
+        isTyping = true;
+
+        if (introText != null)
+            introText.text = "";
 
         foreach (char c in fullText)
         {
-            introText.text += c;
+            if (introText != null)
+                introText.text += c;
+
             yield return new WaitForSecondsRealtime(typingSpeed);
         }
 
@@ -46,12 +105,23 @@ public class IntroTypewriter : MonoBehaviour
 
     public void StartGame()
     {
-        if (isTyping)
+        if (gameStarted)
             return;
+
+        gameStarted = true;
+
+        Debug.Log("START GAME TRIGGERED");
+
+        if (introPanel != null)
+            introPanel.SetActive(false);
+
+        if (titlePanel != null)
+            titlePanel.SetActive(false);
+
+        if (mainCanvas != null)
+            mainCanvas.SetActive(true);
 
         if (gameManager != null)
             gameManager.StartGame();
-
-        introCanvas.SetActive(false);
     }
 }
